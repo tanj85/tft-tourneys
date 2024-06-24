53a5c475b66c6bc51aca25920f518b1aa8827fce
Rachel Jin <racheljin24@gmail.com>
2024-06-24 16:35:20 -0400

    hero section completed

--- frontend/tft-tournaments/app/page.tsx
+++ frontend/tft-tournaments/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
-import Navbar from "./components/navbar";
-
 export default function Home() {
-return(
-  <div className="">
-    Hello World
-    
-  </div>)
+  return (
+    <>
+      {/* hero */}
+      <div className="relative w-full h-[40rem]">
+        <div className="flex items-center justify-center bg-fixed bg-pengu bg-cover h-[40rem] w-full">
+          <div className="z-10 uppercase font-bold text-8xl mix-blend-exclusion">
+            Welcome to TFTourneys
+          </div>
+          <div className="absolute inset-0 bg-black opacity-60 mix-blend-multiply"></div>
+        </div>
+      </div>
+
+      {/* section 1 */}
+      <div className="h-[40rem]">Hello World</div>
+    </>
+  );
 }
