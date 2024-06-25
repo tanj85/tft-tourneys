0f0e2072f9fd49f6644dc16e319ce14e07a1d994
Rachel Jin <racheljin24@gmail.com>
2024-06-25 16:46:20 -0400

    tournament pages started

--- frontend/tft-tournaments/app/data/data.ts
+++ frontend/tft-tournaments/app/data/data.ts
@@ -2,82 +2,85 @@ export const tournamentsExample = [
   {
     id: "1",
     name: "2024 Americas TFT Inkborn Fables Tactician's Cup III",
-    games: {
-      game1: {
-        lobby1: {
-          "So bio#eprod": 1,
-          "prestivent#eprod": 8,
-          "seihunkim#eprod": 7,
-          "ISG FRITZ#eprod": 6,
-          "wilf#eprod": 4,
-          "Sealkun Mbappe#eprod": 2,
-          "Liquid ego#eprod": 5,
-          "Ayy Rick#eprod": 3,
-        },
-        lobby2: {
-          "thybeaster#eprod": 2,
-          "Cambulee#eprod": 5,
-          "LilTop#eprod": 3,
-          "Lab018biribiri#eprod": 1,
-          "Altenahue TFT#eprod": 7,
-          "CookiesOP#eprod": 6,
-          "Ciulla#eprod": 4,
-          "GK Bapzera#eprod": 8,
-        },
+    days: [
+      {
+        games: [
+          {
+            lobbies: [
+              {
+                "So bio#eprod": 1,
+                "prestivent#eprod": 8,
+                "seihunkim#eprod": 7,
+                "ISG FRITZ#eprod": 6,
+                "wilf#eprod": 4,
+                "Sealkun Mbappe#eprod": 2,
+                "Liquid ego#eprod": 5,
+                "Ayy Rick#eprod": 3,
+              },
+              {
+                "thybeaster#eprod": 2,
+                "Cambulee#eprod": 5,
+                "LilTop#eprod": 3,
+                "Lab018biribiri#eprod": 1,
+                "Altenahue TFT#eprod": 7,
+                "CookiesOP#eprod": 6,
+                "Ciulla#eprod": 4,
+                "GK Bapzera#eprod": 8,
+              },
+            ],
+          },
+          {
+            lobbies: [
+              {
+                "So bio#eprod": 8,
+                "prestivent#eprod": 7,
+                "seihunkim#eprod": 5,
+                "ISG FRITZ#eprod": 4,
+                "wilf#eprod": 3,
+                "Sealkun Mbappe#eprod": 1,
+                "Liquid ego#eprod": 2,
+                "Ayy Rick#eprod": 6,
+              },
+              {
+                "thybeaster#eprod": 4,
+                "Cambulee#eprod": 2,
+                "LilTop#eprod": 1,
+                "Lab018biribiri#eprod": 7,
+                "Altenahue TFT#eprod": 6,
+                "CookiesOP#eprod": 3,
+                "Ciulla#eprod": 5,
+                "GK Bapzera#eprod": 8,
+              },
+            ],
+          },
+        ],
+        standings: [
+          {
+            placement: 1,
+            player: "socks#eprod",
+            region: "NA",
+            "games points": [8, 3, 7, 6, 6, 7, 5],
+            total: 42,
+            "cumulative total": 98,
+            "top 4+1": 7,
+            "placement counts": [1, 2, 2, 1, 0, 1, 0, 8],
+            "eod placement": 5,
+            "game placements": [8, 3, 7, 6, 6, 7],
+          },
+          {
+            placement: 2,
+            player: "prestivent#eprod",
+            region: "NA",
+            "games points": [8, 7, 6, 8, 2, 4, 6],
+            total: 41,
+            "cumulative total": 105,
+            "top 4+1": 7,
+            "placement counts": [7, 2, 1, 2, 0, 1, 0, 1, 0],
+            "eod placement": 6,
+            "game placements": [8, 7, 6, 8, 2, 4],
+          },
+        ],
       },
-      game2: {
-        lobby1: {
-          "So bio#eprod": 8,
-          "prestivent#eprod": 7,
-          "seihunkim#eprod": 5,
-          "ISG FRITZ#eprod": 4,
-          "wilf#eprod": 3,
-          "Sealkun Mbappe#eprod": 1,
-          "Liquid ego#eprod": 2,
-          "Ayy Rick#eprod": 6,
-        },
-        lobby2: {
-          "thybeaster#eprod": 4,
-          "Cambulee#eprod": 2,
-          "LilTop#eprod": 1,
-          "Lab018biribiri#eprod": 7,
-          "Altenahue TFT#eprod": 6,
-          "CookiesOP#eprod": 3,
-          "Ciulla#eprod": 5,
-          "GK Bapzera#eprod": 8,
-        },
-      },
-    },
-  },
-  {
-    id: "2",
-    name: "Tourney2",
-  },
-  {
-    id: "3",
-    name: "Tourney3",
+    ],
   },
 ];
-
-//   export const lobbiesExample = [
-//     {
-//         "So bio#eprod": 1,
-//         "prestivent#eprod": 8,
-//         "seihunkim": 7,
-//         "ISG FRITZ#eprod": 6,
-//         "wilf#eprod": 4,
-//         "Sealkun Mbappe#eprod": 2,
-//         "Liquid ego#eprod": 5,
-//         "Ayy Rick#eprod": 3
-//     },
-//     {
-//         "thybeaster#eprod": 2,
-//         "Cambulee#eprod": 5,
-//         "LilTop#eprod": 3,
-//         "Lab018biribiri#eprod": 1,
-//         "Altenahue TFT#eprod": 7,
-//         "CookiesOP#eprod": 6,
-//         "Ciulla#eprod": 4,
-//         "GK Bapzera#eprod": 8
-//     }
-// ]
