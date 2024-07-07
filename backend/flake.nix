921a64c68c2780b845e6a6cf3e8528762a18fb51
Tean Lai <laitean19@gmail.com>
2024-07-06 22:53:09 -0400

    tourneys show dates

---
+++ backend/flake.nix
@@ -0,0 +1,16 @@
+{
+  description "python environment for tftourneys backend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.x84_64-linux.default = (import ./shell.nix { inherit pkgs; });
+  };
+}
